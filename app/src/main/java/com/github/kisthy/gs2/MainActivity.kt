44bdd5d8d050510d3992e916e17b548553e67eb1
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-18 21:47:22 -0300

    first commit

---
+++ app/src/main/java/com/github/kisthy/gs2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.github.kisthy.gs2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.github.kisthy.gs2.database.repository.EcoTipRepository
+import com.github.kisthy.gs2.screens.EcoTipScreen
+import com.github.kisthy.gs2.screens.EcoTipsScreenViewModel
+import com.github.kisthy.gs2.ui.theme.Matheus_RM96272Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val repository = EcoTipRepository(applicationContext)
+        val viewModel = EcoTipsScreenViewModel(repository)
+        setContent {
+            Matheus_RM96272Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    EcoTipScreen(viewModel = viewModel)
+                }
+            }
+        }
+    }
+}
